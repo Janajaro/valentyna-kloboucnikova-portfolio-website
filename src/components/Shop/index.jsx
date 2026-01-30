97245809b6b6aa93d0575cae411d7ada9eaa9834
Jana Jarošová <janamjaros@seznam.cz>
2026-01-30 23:15:30 +0100

    change of Shop component

--- src/components/Shop/index.jsx
+++ src/components/Shop/index.jsx
@@ -3,31 +3,25 @@ import { IndividualGallery } from '../IndividualGallery';
 import { individualProjects } from "../../data.js"
 
 export const Shop = () => {
-
-    const shopProjects = individualProjects.filter((item) => item.buy !== "")
-
-  return (
-
-    <div className="gallery">
-      {
-        shopProjects.map((singleItem) => (
-          <a 
-            href={singleItem.buy}
-            target='_blank'
-            rel="noopener noreferrer"
-            key={singleItem.id}
-          >
-
-              <IndividualGallery
-                img={singleItem.image}
-                shrtDesc={singleItem.shortDescription}
-              />
-
-          </a>
-        ))
-      }
-    </div>
-
+  const shopProjects = individualProjects.filter(
+    (item) => typeof item.buy === "string" && item.buy.trim() !== ""
   );
 
-}
+  return (
+    <div className="gallery">
+      {shopProjects.map((singleItem) => (
+        <a
+          href={singleItem.buy.trim()}
+          target="_blank"
+          rel="noopener noreferrer"
+          key={singleItem.id}
+        >
+          <IndividualGallery
+            img={singleItem.image}
+            shrtDesc={singleItem.shortDescription}
+          />
+        </a>
+      ))}
+    </div>
+  );
+};
