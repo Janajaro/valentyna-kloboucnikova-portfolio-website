3cd8e4fb28fa75ae278e2581aab530e6ebde3239
Jana Jarošová <janamjaros@seznam.cz>
2026-02-16 22:02:08 +0100

    CMS update about section

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,11 +1,12 @@
 import './index.css';
+import about from "../../data/about.json"
 
 export const About = () => {
     return (
         <>
             <div className="container">
-                <div>While this exhibition stimulated debate about the place of women artists in the art world, it also defined them as separate from their canonized male counterparts and reinforced the damaging notion of a universally feminine experience. Commenting on her reception as a woman artist in the 1940s, Bourgeois said that she doesn't "know what art made by a woman is....There is no feminine experience in art, at least not in my case, because not just by being a woman does one have a different experience." </div>
-                <img src="/img/chrt (2).png" alt="Valentyna-Kloboucnikova-portret" />
+                <div>{about.text}</div>
+                {about.image && <img src={about.image} alt={about.imageAlt || ""}></img>}
             </div>
         </>
     )
